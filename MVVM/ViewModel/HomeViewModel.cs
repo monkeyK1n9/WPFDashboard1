270cebd32cbfd237872cb17f27b33ad9f87970ac
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-19 13:16:47 +0100

    Embed home view in main window

---
+++ MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDashboard1.MVVM.ViewModel
+{
+    class HomeViewModel
+    {
+        public HomeViewModel()
+        {
+
+        }
+    }
+}
--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,24 @@ namespace WPFDashboard1.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        public HomeViewModel Home { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChange();
+            }
+        }
+
         public MainViewModel()
         {
-            
+            Home = new HomeViewModel();
+            CurrentView = Home;
         }
     }
 }
